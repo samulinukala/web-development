a3efd0dcebae3e374e9a3aa1c3f43f44ed1fb3f5
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-09-30 08:36:18 +0300

    update phone site/phonescript.js

--- phone site/phonescript.js
+++ phone site/phonescript.js
@@ -1,11 +1,16 @@
 window.onload=function(){randomiseColor};
 function randomiseColor(){
-  while(true){
+  
   let red= Math.floor( Math.random()*255);
   let blue= Math.floor( Math.random()*255);
   let green= Math.floor( Math.random()*255);
   let color= "rgb(" +red+","+green+","+blue+")";
  document.getElementById("bg").style.backgroundColor= color;
-  }
+
+  
  console.log("completed");
+}
+function loopColor()
+{
+  setTimeout(randomiseColor(),3000);
 }
