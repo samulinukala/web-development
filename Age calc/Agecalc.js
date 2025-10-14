69746bd48a8b0515b09ad77fd74c51c1553b9c78
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-14 12:06:16 +0300

    update Age calc/Agecalc.js

--- Age calc/Agecalc.js
+++ Age calc/Agecalc.js
@@ -2,6 +2,6 @@ function calculateAge(){
 const curDate=new Date();
 const d= curDate.getFullYear();
 const bday=document.getElementById("bday").value;
-document.getElementById("p1").innerHTML=bday;
+document.getElementById("p1").innerHTML=works;
 console.log(bday);
 }
