531403c5b9d03c5ea37a07896a3141d2ac2a5052
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-17 13:29:15 +0300

    update Java testing/Java testing.js

--- Java testing/Java testing.js
+++ Java testing/Java testing.js
@@ -8,7 +8,7 @@ do {
   const second=time.getSeconds();
   const minute=time.getMinutes();
   const millisecond=time.getMilliseconds();
-  clockText.innerText=hour+" : "+
+  clockText.innerText=hour+" : "+minute
 }
 while (true);
 
