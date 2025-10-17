b0844c0bbc87a34b71ae4d11f284a88cd722ff36
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-17 13:26:34 +0300

    update Java testing/Java testing.js

--- Java testing/Java testing.js
+++ Java testing/Java testing.js
@@ -4,7 +4,8 @@ const clockText=document.getElementById("clockText");
 
 do {
   let time=new Date();
-  const hour=time.get
+  const hour=time.getHours;
+  const seconds
   clockText.innerText=
 }
 while (true);
