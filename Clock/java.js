ee74a794dfe383c7f6e11df0a27d44fd47af309f
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-17 15:04:32 +0300

    update Clock/java.js

--- Clock/java.js
+++ Clock/java.js
@@ -3,7 +3,7 @@ function clock()
 const clockText=document.getElementById("clockText"); 
   
   let time=new Date();
-  let display
+  let display=
   clockText.innerText=hour+":"+minute+ ":"+ second;
 }
 function loopClock()
