6f1507365fca8a1974bcdb4093e59b81348b8890
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-27 16:03:45 +0200

    update Calculator/Calc.js

--- Calculator/Calc.js
+++ Calculator/Calc.js
@@ -7,7 +7,7 @@ let num1=0;
 let num2=0;
 let num3=0;
 let whichnumber=false;
-const result=document.getElementById()
+const result=document.getElementById("result")
 function addnumber(numd){
   if(whichnumber==false){
     num1=num1+numd;
