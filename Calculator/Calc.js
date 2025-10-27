8f70cd75a1bee0c11ec41ab2e547b7ab71791347
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-27 16:14:57 +0200

    update Calculator/Calc.js

--- Calculator/Calc.js
+++ Calculator/Calc.js
@@ -1,14 +1,13 @@
 function setUp()
 
 {
-  
-  
-}
-let num1=0;
+   let num1=0;
 let num2=0;
 let num3=0;
 let whichnumber=false;
 const result=document.getElementById("result");
+}
+
 let op="";
 
 function addnumber(numd){
