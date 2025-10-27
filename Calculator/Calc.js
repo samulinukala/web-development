dde477bdf8caaeaa5aacd0d769b59c8263fe8197
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-27 16:14:05 +0200

    update Calculator/Calc.js

--- Calculator/Calc.js
+++ Calculator/Calc.js
@@ -9,13 +9,14 @@ let num3=0;
 let whichnumber=false;
 const result=document.getElementById("result");
 let op="";
+
 function addnumber(numd){
   if(whichnumber==false){
     num1=num1+numd;
     result.innerHTML=num1;
   }else{
     num2=num2+numd;
-    result.innerText
+    result.innerText=num2;
   }
 }
 function operation(sign){
