f9da44731e9e4e700ab99f3f727038d0f2e76cd5
Samuli Nukala <71631113+samulinukala@users.noreply.github.com>
2025-10-28 12:56:46 +0200

    update Calculator/Calc.js

--- Calculator/Calc.js
+++ Calculator/Calc.js
@@ -1,10 +1,13 @@
+function setUp()
 
+{
 let num1=0;
 let num2=0;
 let num3=0;
 let whichnumber=false;
 const result=document.getElementById("result");
 let op="";
+}
 
 function addNumber(numd){
   if(whichnumber==false){
